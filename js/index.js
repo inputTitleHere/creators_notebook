71e86feb2f25c8516ee9b57de250aca3a024ad0a
inputTitleHere <seanbryan97@naver.com>
2022-06-09 12:23:42 +0900

    Added Magic Layout at index login button position

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+
+window.addEventListener('load',()=>{
+  alignMagicCircles();
+});
+
+const alignMagicCircles=()=>{
+  const outwards = document.querySelectorAll('.outwards');
+  outwards.forEach((item)=>{
+    const [ , className] = item.classList;
+    console.log(className);
+    switch (className) {
+      case "tl":
+        item.style.transform="translate(-50px, -50px)";
+        return;
+      case "tr":
+        item.style.transform="translate(50px, -50px)";
+        return;
+      case "bl":
+        item.style.transform="translate(-50px, 50px)";
+        return;
+      case "br":
+        item.style.transform="translate(50px, 50px)";
+        return;
+    }
+
+  })
+};
+
+
